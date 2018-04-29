16415f821ad7662bcbd754d84b73ecc3f08e473d
Vinitadashoralk <vinitadashora.lk@gmail.com>
2018-04-29 20:24:25 +0530

    read data from remote api in every 2 minutes and update contents in mongodb

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var request = require("request");
+var cron = require("node-cron");
 var port = process.env.PORT || 3000;
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/rankmeOnlineDb";
@@ -10,13 +12,32 @@ MongoClient.connect(url, (err, db) => {
  	var sortdata = {$natural: -1};
  	console.log("database connected");
  	app.get("/", (req, res) => {
-		
 		rankmeOnlineDbObj.collection("rankmeOnlineDb").find({}).limit(2).sort(sortdata).toArray((err, lastRecord) => {
 			if(err) throw err;
 			res.send(lastRecord);
 		});
-
 	});
+
+	cron.schedule('*/1 * * * *', () =>{
+		request.get("https://blog.rankmeonline.com/wp-json/wp/v2/posts", (error, response, body) =>{
+			if(error){
+				return console.dir(error);
+			}
+			var reqJsonDataObj = JSON.parse(body);
+			reqJsonDataObj.forEach((reqJsonDataElement) =>{
+
+				var JsonDataArray = {
+						"date" : reqJsonDataElement.date, 
+						"title" : reqJsonDataElement.title
+						}; 
+
+				rankmeOnlineDbObj.collection("rankmeOnlineDb").insert(JsonDataArray, (err, res) => {
+					if(err) throw err;
+					console.log("inserted");
+				});
+			});
+	 	});
+ 	})
 });
 
 app.listen(port, () =>{
