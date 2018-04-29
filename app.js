b1ea163656cececc1a210338e74977bb0254de9b
Vinitadashoralk <vinitadashora.lk@gmail.com>
2018-04-29 17:38:37 +0530

    code to fetch last 2 record from rankmeOnlineDb

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ var app = express();
 var port = process.env.PORT || 3000;
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/rankmeOnlineDb";
-app.get("/", (req, res) => {
-	res.send("hello");
-});
 
 MongoClient.connect(url, (err, db) => {
  	if(err) throw err;
+ 	var rankmeOnlineDbObj = db.db("rankmeOnlineDb");
+ 	var sortdata = {$natural: -1};
  	console.log("database connected");
- 	db.close();
+ 	app.get("/", (req, res) => {
+		
+		rankmeOnlineDbObj.collection("rankmeOnlineDb").find({}).limit(2).sort(sortdata).toArray((err, lastRecord) => {
+			if(err) throw err;
+			res.send(lastRecord);
+		});
+
+	});
 });
 
 app.listen(port, () =>{
